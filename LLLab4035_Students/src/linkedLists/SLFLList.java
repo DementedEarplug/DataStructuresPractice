341e5b4506eeb22164ece01868ac8f49f4509587
Onix <onix.tarrats@upr.edu>
2017-08-05 11:02:02 -0400

    The Real LLLAb complete

--- LLLab4035_Students/src/linkedLists/SLFLList.java
+++ LLLab4035_Students/src/linkedLists/SLFLList.java
@@ -9,97 +9,152 @@ package linkedLists;
 
 import linkedLists.LinkedList;
 
-public class SLFLList<E> 
-implements LinkedList<E>
+public class SLFLList<E> implements LinkedList<E>
 {
 
-	private SNode<E> first, last; 
-	int length = 0; 
-	
-	public SLFLList() { 
-		first = last = null; 
+	private SNode<E> first, last;
+	int length;
+
+	public SLFLList()
+	{
+		first = last = null;
+		length=0;
 	}
+
+	public void addFirstNode(Node<E> nuevo)
+	{
+		SNode<E> nuevoX= (SNode<E>) nuevo;
+		if(length()==0)
+			first=nuevoX;
+		nuevoX.setNext(first);
+		first= nuevoX;
+		length++;
+
+	}
+
+	public void addNodeAfter(Node<E> target, Node<E> nuevo)
+	{
+		//Precondition target is a valid node.
+		SNode<E> sNuevo= (SNode<E>) nuevo;
+		SNode<E> sTarget= (SNode<E>) target;
 	
-	
-	public void addFirstNode(Node<E> nuevo) {
-		// TODO Auto-generated method stub
+		sNuevo.setNext((SNode<E>)getNodeAfter(sTarget));
+		sTarget.setNext(sNuevo);
+		
+		length++;		
+	}
+
+	public void addNodeBefore(Node<E> target, Node<E> nuevo)
+	{
+		SNode<E> sNuevo= (SNode<E>) nuevo;
+		SNode<E> sTarget= (SNode<E>) target;
+		SNode<E> marker= (SNode<E>)getNodeBefore(sTarget);
+		
+		sNuevo.setNext(sTarget);
+		marker.setNext(sNuevo);
+		
+		length++;
+	}
+
+	public Node<E> getFirstNode() throws NodeOutOfBoundsException
+	{
+		if(length()==0)
+			throw new NodeOutOfBoundsException("Dude this is empty...");
+		
+		return first;
+	}
+
+	public Node<E> getLastNode() throws NodeOutOfBoundsException
+	{
+		if(length()==0)
+			throw new NodeOutOfBoundsException("Dude this is empty...");
+		
+		SNode<E> marker = first;
+		while(marker.getNext()!=null)
+			marker=marker.getNext();
+		
+		return marker;
+	}
+
+	public Node<E> getNodeAfter(Node<E> target) throws NodeOutOfBoundsException
+	{
+		SNode<E> sTarget= (SNode<E>)target;
+		return sTarget.getNext();
+	}
+
+	public Node<E> getNodeBefore(Node<E> target) throws NodeOutOfBoundsException
+	{
+		SNode<E> marker= first;
+		while(marker.getNext()!=target)
+			marker = marker.getNext();
+		return marker;
+	}
+
+	public int length()
+	{
+		return length;
+	}
+
+	public void removeNode(Node<E> target)
+	{
+		SNode<E> sTarget = (SNode<E>)target;
+		SNode<E> after = (SNode<E>)getNodeAfter(target);
+		SNode<E> before = (SNode<E>)getNodeBefore(target);
+		
+		before.setNext(after);
+		sTarget.setNext(null);
+		length--;
 		
 	}
 
-	public void addNodeAfter(Node<E> target, Node<E> nuevo) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void addNodeBefore(Node<E> target, Node<E> nuevo) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public Node<E> getFirstNode() throws NodeOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Node<E> getLastNode() throws NodeOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Node<E> getNodeAfter(Node<E> target) throws NodeOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Node<E> getNodeBefore(Node<E> target)
-			throws NodeOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int length() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public void removeNode(Node<E> target) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public Node<E> createNewNode() {
+	public Node<E> createNewNode()
+	{
 		return new SNode<E>();
 	}
 
-
 	///////////////////////////////////////////////////
-	// private and static inner class that defines the 
+	// private and static inner class that defines the
 	// type of node that this list implementation uses
-	private static class SNode<T> implements Node<T> {
-		private T element; 
-		private SNode<T> next; 
-		public SNode() { 
-			element = null; 
-			next = null; 
+	private static class SNode<T> implements Node<T>
+	{
+		private T element;
+		private SNode<T> next;
+
+		public SNode()
+		{
+			element = null;
+			next = null;
 		}
-		public SNode(T data, SNode<T> next) { 
-			this.element = data; 
-			this.next = next; 
+
+		public SNode(T data, SNode<T> next)
+		{
+			this.element = data;
+			this.next = next;
 		}
-		public SNode(T data)  { 
-			this.element = data; 
-			next = null; 
+
+		public SNode(T data)
+		{
+			this.element = data;
+			next = null;
 		}
-		public T getElement() {
+
+		public T getElement()
+		{
 			return element;
 		}
-		public void setElement(T data) {
+
+		public void setElement(T data)
+		{
 			this.element = data;
 		}
-		public SNode<T> getNext() {
+
+		public SNode<T> getNext()
+		{
 			return next;
 		}
-		public void setNext(SNode<T> next) {
+
+		public void setNext(SNode<T> next)
+		{
 			this.next = next;
 		}
 	}
